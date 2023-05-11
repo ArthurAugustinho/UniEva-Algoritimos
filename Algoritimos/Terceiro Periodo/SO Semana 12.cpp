862a6263b0d534de44cc8fee70f4bfbb019db192
Arthur Augustinho Dos Anjos <98282637+ArthurAugustinho@users.noreply.github.com>
2023-05-10 21:30:06 -0300

    Add files via upload

---
+++ Algoritimos/Terceiro Periodo/SO Semana 12.cpp
@@ -0,0 +1,25 @@
+/*PROGRAMA DE ALOCAÇÃO DINAMICA DE MEMORIA
+GERENCIAMENTO DE MEMORIA*/
+
+#include<iostream>
+using namespace std;
+
+int main(){
+	int n=10, i, *ptr, sum=0;
+	
+	ptr = new int[n]; //aloca a memoria dinamicamente
+	
+	if(ptr == NULL){
+		cout << "Erro! Memoria nao alocada";
+		exit(0);
+	}
+	cout << "Digite os " << n << " elementos: \n";
+	for(i=0; i<10; i++){
+		cin >> *(ptr + i);
+		sum += *(ptr + i); // sum = sum + *(ptr + i)
+	}
+	
+	cout << "A soma dos cadastrados e: " <<sum<<endl;
+	
+	delete[] ptr; //libera a memoria alocada
+}
